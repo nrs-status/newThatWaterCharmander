e300c70c1001000b943f6ca9a1be20ada95bec05
nrs-status <sebaarsim@gmail.com>
2026-08-25 08:09:08 +0000

    fixing

--- zeusOlympia/virtualization.nix
+++ zeusOlympia/virtualization.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+      };
     };
-  };
-  podman = {
-    enable = true;
-    dockerCompat = true;
-    defaultNetwork.settings.dns_enabled = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
   };
 }
