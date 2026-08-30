434d995f64699651fe81ea0ced4531ae1a3c10e0
nrs-status <sebaarsim@gmail.com>
2026-08-30 17:29:15 +0000

    fixing scratchpad

--- zeusOlympia/sway/setupWorkspaces.nix
+++ zeusOlympia/sway/setupWorkspaces.nix
@@ -14,7 +14,7 @@ pkgs.writeShellApplication {
     WIDTH=1366
     HEIGHT=765
 
-    swaymsg exec ${pkgsLib.getExe pkgs.kitty}
+    swaymsg exec ${pkgsLib.getExe pkgs.kitty} nix develop ${frontArmToPlane}#sieyes
     sleep 0.5 # Wait for the new window to appear and gain focus
     swaymsg resize set width "$WIDTH" height "$HEIGHT"
     sleep 0.2 # Small delay to let the resize apply before moving off-screen
