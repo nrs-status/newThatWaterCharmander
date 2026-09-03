6735cdd5febdef512b0ff9a700c8ea7cba1227ad
nrs-status <sebaarsim@gmail.com>
2026-09-03 03:00:02 +0000

    fixing sops

--- zeusOlympia/wranHearst/sops.nix
+++ zeusOlympia/wranHearst/sops.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   pkgsLib,
   frontArmToPlane,
   sopsFlake,
@@ -8,26 +7,23 @@
 # the general idea for handling secrets is as follows:
 # 1. the secrets are a nix package somewhere and get pulled and set as `defaultSopsFile` (it is possible to do a per-secret configuration that doesn't require setting `defaultSopsFile`
 # 2. *the host must manage which users get access to which secret!* this is done locally and is a host-specific configuration
-pkgsLib.mkMerge [
-  sopsFlake.nixosModules.sops
-
-  {
+{
+  imports = [ sopsFlake.nixosModules.sops ];
+  sops = {
+    defaultSopsFile = "${frontArmToPlane.packages.x86_64-linux.secrets}/secrets.yaml";
     sops = {
-      defaultSopsFile = "${frontArmToPlane.packages.x86_64-linux.secrets}/secrets.yaml";
-      sops = {
-        age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        secrets = {
-          OPENROUTER_API_KEY = {
-            owner = "sieyes";
-            mode = "0400";
-          };
-          GITHUB_API_KEY = {
-            owner = "sieyes";
-            mode = "0400";
-          };
+      age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      secrets = {
+        OPENROUTER_API_KEY = {
+          owner = "sieyes";
+          mode = "0400";
+        };
+        GITHUB_API_KEY = {
+          owner = "sieyes";
+          mode = "0400";
         };
       };
-
     };
-  }
-]
+
+  };
+}
