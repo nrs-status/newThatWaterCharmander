48e2328ea408e3396680876976d7e09d326143b8
nrs-status <sebaarsim@gmail.com>
2026-09-04 19:44:20 +0000

    disable password auth for wranHearst openssh

--- zeusOlympia/wranHearst/networking.nix
+++ zeusOlympia/wranHearst/networking.nix
@@ -5,5 +5,8 @@
     hostName = pkgs.lib.mkDefault "wranHearst";
     useDHCP = pkgs.lib.mkDefault true;
   };
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
 }
