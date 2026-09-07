ed474c1b735122f17213b899dc2b37532f48456b
nrs-status <sebaarsim@gmail.com>
2026-09-07 08:03:39 +0000

    moving meta from d to a

--- zeusOlympia/keyRemappings.nix
+++ zeusOlympia/keyRemappings.nix
@@ -6,14 +6,19 @@
         ids = [ "*" ];
         settings = {
           main = {
-            leftcontrol = "0";
+            leftcontrol = "capslock";
             rightcontrol = "layer(custom)";
-            capslock = "leftcontrol";
+            capslock = "layer(custom2)";
             meta = "layer(custom2)";
+            "a" = "overloadt(meta, a, 130)";
+            "s" = "overloadt(leftalt, s, 130)";
+            "d" = "overloadt(leftcontrol, d, 130)";
             "f" = "overloadt(shift, f, 130)";
+
             "h" = "overloadt(shift, h, 130)";
-            "j" = "overloadt(meta, j, 130)";
-            "d" = "overloadt(meta, d, 130)";
+            "j" = "overloadt(rightcontrol, j, 130)";
+            "k" = "overloadt(rightalt, k, 130)";
+            "l" = "overloadt(meta, l, 130)";
 
             "1" = "!";
             "2" = "@";
