14964d1043eabe340fc58a5302198a2ae7e5e4a0
nrs-status <sebaarsim@gmail.com>
2026-09-10 12:35:15 +0000

    added authorized SSH key to lanchamarcou

--- zeusOlympia/lanchamarcou/ssh.nix
+++ zeusOlympia/lanchamarcou/ssh.nix
@@ -5,5 +5,14 @@
       PasswordAuthentication = false;
     };
   };
+
+  #the current host's own ssh host key is used as the authorized key for ssh
+  #access, for both plat2548 and root
+  users.users.plat2548.openssh.authorizedKeys.keys = [
+    (builtins.readFile ./sshHostKey.pub)
+  ];
+  users.users.root.openssh.authorizedKeys.keys = [
+    (builtins.readFile ./sshHostKey.pub)
+  ];
   networking.firewall.allowedTCPPorts = [ 22 ];
 }
