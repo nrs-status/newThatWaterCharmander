7ed1acd94fece6e406a2b926d6561f531ab07300
nrs-status <sebaarsim@gmail.com>
2026-09-10 04:04:23 +0000

    made shell cache service use store-pathed `nix`

--- heidRunOverCar/mkShellCachingModule.nix
+++ heidRunOverCar/mkShellCachingModule.nix
@@ -1,11 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 shell:
 {
   systemd.user.services."cache${shell.name}Shell" = {
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "nix build --expr ${shell.inputDerivation} --no-link --print-out-paths";
+      ExecStart = "${pkgs.lib.getExe pkgs.nix} build --expr ${shell.inputDerivation} --no-link --print-out-paths";
 
       #don't compete with rest of login sequence
       Nice = 19;
