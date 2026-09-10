e692b12f1fc23cf4d922d0adde01530b473d2e7a
nrs-status <sebaarsim@gmail.com>
2026-09-10 02:51:11 +0000

    fixing bad keyd mapping

--- zeusOlympia/keyRemappings.nix
+++ zeusOlympia/keyRemappings.nix
@@ -8,7 +8,7 @@
         settings = {
           main = {
             leftcontrol = "capslock";
-            rightalt = "F13"; #F13 is unused otherwise; this binding is for activating voice transcription
+            rightalt = "f13"; #keyd key names are lowercase; emitting evdev f13 (keycode 191), which sway sees as keysym F13. F13 is unused otherwise; this binding is for activating voice transcription
             capslock = "layer(custom2)";
             meta = "layer(custom2)";
             "a" = "overloadt(meta, a, 130)";
