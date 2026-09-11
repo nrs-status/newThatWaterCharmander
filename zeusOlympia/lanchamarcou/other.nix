c184f8618fc89126b8b0a52ac8900b7b8154424a
nrs-status <sebaarsim@gmail.com>
2026-09-11 09:31:22 +0000

    adding hashed pwds to lanchamarcou

--- zeusOlympia/lanchamarcou/other.nix
+++ zeusOlympia/lanchamarcou/other.nix
@@ -1,8 +1,8 @@
 {
   system.stateVersion = "26.11";
 
-  # root password is declared here rather than set imperatively (e.g. via
-  # chpasswd) because the impermanence rollback wipes the root subvolume on
-  # every boot, resetting /etc/shadow to the pristine state
-  users.users.root.hashedPassword = "$6$xyx9wiTbAipINj/9$8YR9WsLY.RoYhV7cVufX9vvxzp.dc9eoHSVAb.55xoxabKb3JnIg8DwtUeNK36sZlR5ngL.HT7TFCDP0HF7EU.";
+  users.users = {
+    root.hashedPassword = "$y$j9T$wnwHc8mBfXipYGO2m.Ui50$YJXapso8R.AlpPbVHfYGlEH9k2kuM5vTLiZBj1rHcA/";
+    plat2548.hashedPassword = "$y$j9T$wnwHc8mBfXipYGO2m.Ui50$YJXapso8R.AlpPbVHfYGlEH9k2kuM5vTLiZBj1rHcA/";
+  };
 }
