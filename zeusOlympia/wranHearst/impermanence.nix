c83e6527b4f7f7e9341e8bc36e43eb2ef66670c5
nrs-status <sebaarsim@gmail.com>
2026-09-14 23:44:07 +0000

    fixing permissions

--- zeusOlympia/wranHearst/impermanence.nix
+++ zeusOlympia/wranHearst/impermanence.nix
@@ -7,6 +7,8 @@
         "daguerreBrick"
         "smithShirtCube"
         "kierLeapMount"
+
+        #persisting entire state directories to handle everything the same way. can sometimes do file-by-file but e.g. `zoxide` tries to create a new file and then move it to `.local/share/zoxide/db.zo`, which is not allowed if we declare it as an `impermanence` file, because it is then mounted (so renaming that file or `mv`ing another file over it is disallowed)
         ".local/share/zoxide"
         ".local/share/atuin"
       ];
