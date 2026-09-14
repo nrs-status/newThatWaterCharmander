2981b32c10ba9654042368358c6149c9e6f5b710
nrs-status <sebaarsim@gmail.com>
2026-09-14 01:27:00 +0000

    reading wranHearstPublicKey from host ssh key instead of hardcoding

--- zeusOlympia/global.nix
+++ zeusOlympia/global.nix
@@ -2,7 +2,7 @@
 {
   options.wranHearstPublicKey = pkgsLib.mkOption { };
   config = {
-    wranHearstPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+Zkx82s6OTrx6Wk4qwC1b9RyiuTFBMW6MZHm32uKyJ";
+    wranHearstPublicKey = builtins.readFile /etc/ssh/ssh_host_ed25519_key.pub;
     environment = {
       variables = {
         EDITOR = "nvim";
