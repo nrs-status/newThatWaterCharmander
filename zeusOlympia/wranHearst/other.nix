79902019f47226183dd150bed2fb83ab00cc1aa3
nrs-status <sebaarsim@gmail.com>
2026-09-14 13:27:24 +0000

    adding hashedpassword  to wranHearst

--- zeusOlympia/wranHearst/other.nix
+++ zeusOlympia/wranHearst/other.nix
@@ -1,3 +1,8 @@
 {
   system.stateVersion = "26.11";
+
+  users.users = {
+    root.hashedPassword = "$y$j9T$m4Vx2ODhybLUlsKC9NyB30$L01GM5ntWJafyLQ3O9hyktv7vKgqMGE93RmwT8kbs96";
+    sieyes.hashedPassword = "$y$j9T$m4Vx2ODhybLUlsKC9NyB30$L01GM5ntWJafyLQ3O9hyktv7vKgqMGE93RmwT8kbs96";
+  };
 }
