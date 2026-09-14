be6f3bf50fc452045df2c1ce2dccd6a6a5a6e863
nrs-status <sebaarsim@gmail.com>
2026-09-14 05:48:29 +0000

    adding the public key for the nix community substitutor and adding a a directory for SSH keys to the wranHearst host.

--- zeusOlympia/nix.nix
+++ zeusOlympia/nix.nix
@@ -15,6 +15,7 @@
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" # nix-community
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" # key for binary cache for haskell.nix
       ]; 
     };
--- zeusOlympia/wranHearst/impermanence.nix
+++ zeusOlympia/wranHearst/impermanence.nix
@@ -6,6 +6,7 @@
         "baghdadPlane"
         "daguerreBrick"
         "smithShirtCube"
+        "kierLeapMount"
       ];
     };
   };
