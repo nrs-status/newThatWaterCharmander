6a2b1ad9d6e387fce0d4aa01c0f1e6896ae239be
nrs-status <sebaarsim@gmail.com>
2026-09-14 05:18:22 +0000

    Squash commits from default-to-headless-shell
    Combined commits:
    - Committing the changes in default shells and previous metadata from agentic run
    - removing metadata

--- zeusOlympia/augtibcalcla/soc7099.nix
+++ zeusOlympia/augtibcalcla/soc7099.nix
@@ -1,16 +1,20 @@
 #this user is inside a host config because unlike sieyes it isn't meant to be used anywhere else than on the `augtibcalcla`
-{ pkgs, ... }:
-{
-  users.users.soc7099 = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "docker"
-      "networkmanager"
-      "audio"
-      "video"
-    ];
-    packages = with pkgs; [ zoxide btop ];
-  };
+{ pkgs, pkgsLib, localLib, frontArmToPlane, ... }:
+pkgsLib.mkMerge [
+  #pre-warm the host's shell (`headless`) so terminals open instantly after a rebuild
+  (localLib.mkShellCachingModule { inherit pkgs; shell = frontArmToPlane.devShells.x86_64-linux.headless; })
 
-}
+  {
+    users.users.soc7099 = {
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "docker"
+        "networkmanager"
+        "audio"
+        "video"
+      ];
+      packages = with pkgs; [ zoxide btop ];
+    };
+  }
+]
