4a6db7a7be1d0dd79e86495b07fe48a703a8e933
nrs-status <sebaarsim@gmail.com>
2026-09-14 02:36:20 +0000

    renaming plat7099 user to soc7099

--- zeusOlympia/augtibcalcla/other.nix
+++ zeusOlympia/augtibcalcla/other.nix
@@ -2,6 +2,6 @@
   system.stateVersion = "26.11";
   users.users = {
     root.hashedPassword = "$y$j9T$Oo.6EIzF0nHn5caL9b4RM0$sLXfIoJ1eXiaZoSROOqT8QkmGvNw3vNj/O8/cZ0F0C3";
-    plat7099.hashedPassword = "$y$j9T$Oo.6EIzF0nHn5caL9b4RM0$sLXfIoJ1eXiaZoSROOqT8QkmGvNw3vNj/O8/cZ0F0C3";
+    soc7099.hashedPassword = "$y$j9T$Oo.6EIzF0nHn5caL9b4RM0$sLXfIoJ1eXiaZoSROOqT8QkmGvNw3vNj/O8/cZ0F0C3";
   };
 }
