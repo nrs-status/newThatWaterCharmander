602bc9cf3ed292500614698cf492420e2eafb0d7
nrs-status <sebaarsim@gmail.com>
2026-09-14 01:30:58 +0000

    nesting EDITOR under environment.variables in global config

--- zeusOlympia/global.nix
+++ zeusOlympia/global.nix
@@ -3,8 +3,10 @@
   options.wranHearstPublicKey = pkgsLib.mkOption { };
   config = {
     wranHearstPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+Zkx82s6OTrx6Wk4qwC1b9RyiuTFBMW6MZHm32uKyJ root@nixos";
-    variables = {
-      EDITOR = "nvim";
+    environment = {
+      variables = {
+        EDITOR = "nvim";
+      };
     };
   };
 }
