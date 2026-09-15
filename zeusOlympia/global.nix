6386a6642d7ecd9a9ae68c6bdd47c507695d4f82
nrs-status <sebaarsim@gmail.com>
2026-09-15 00:31:38 +0000

    rotating wranHearst public key

--- zeusOlympia/global.nix
+++ zeusOlympia/global.nix
@@ -2,7 +2,7 @@
 {
   options.wranHearstPublicKey = pkgsLib.mkOption { };
   config = {
-    wranHearstPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+Zkx82s6OTrx6Wk4qwC1b9RyiuTFBMW6MZHm32uKyJ root@nixos";
+    wranHearstPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+gHdBdYnmkKNQbt9YeSRjomS+eoFEfmusiZ6ooGBEI";
     environment = {
       variables = {
         EDITOR = "nvim";
