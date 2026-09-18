f867b63afa4c540e32a47a439de65a15d4663fc6
nrs-status <sebaarsim@gmail.com>
2026-09-18 00:26:43 -0400

    re-enabling shellCacher in wranHearst-minimal

--- empTriageCan/wranHearst-minimal/users.nix
+++ empTriageCan/wranHearst-minimal/users.nix
@@ -1,10 +1,10 @@
 { frontArmToPlane, localModules, ... }:
 {
 
-  # imports = with localModules; [ shellCacher ];
-  # shellCacher.shell = frontArmToPlane.devShells.x86_64-linux.sieyes;
-  # # last-resort fallback for the shellCacher launcher (see zeusOlympia/shellCacher.nix)
-  # shellCacher.flakeRef = "frontArmToPlane#sieyes";
+  imports = with localModules; [ shellCacher ];
+  shellCacher.shell = frontArmToPlane.devShells.x86_64-linux.sieyes;
+  # last-resort fallback for the shellCacher launcher (see zeusOlympia/shellCacher.nix)
+  shellCacher.flakeRef = "frontArmToPlane#sieyes";
 
   users.users = {
     root.hashedPassword = "$y$j9T$m4Vx2ODhybLUlsKC9NyB30$L01GM5ntWJafyLQ3O9hyktv7vKgqMGE93RmwT8kbs96";
