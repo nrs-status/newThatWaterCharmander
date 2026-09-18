e863bc8d48d627e0f83463078b07d1570cb0cdaf
nrs-status <sebaarsim@gmail.com>
2026-09-18 07:21:35 -0400

    disabling shellCacher in augtibcalcla

--- empTriageCan/augtibcalcla/users.nix
+++ empTriageCan/augtibcalcla/users.nix
@@ -2,10 +2,10 @@
 {
   imports = [ localModules.shellCacher ];
 
-  shellCacher.shell = frontArmToPlane.devShells.x86_64-linux.headless;
-  # last-resort fallback for the shellCacher launcher (see zeusOlympia/shellCacher.nix)
-  shellCacher.flakeRef = "frontArmToPlane#headless";
-
+  # shellCacher.shell = frontArmToPlane.devShells.x86_64-linux.headless;
+  # # last-resort fallback for the shellCacher launcher (see zeusOlympia/shellCacher.nix)
+  # shellCacher.flakeRef = "frontArmToPlane#headless";
+  #
   users.users = {
     root.hashedPassword = "$y$j9T$Oo.6EIzF0nHn5caL9b4RM0$sLXfIoJ1eXiaZoSROOqT8QkmGvNw3vNj/O8/cZ0F0C3";
     soc7099 = {
